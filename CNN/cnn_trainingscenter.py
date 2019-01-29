89bcbd5cfd70deb539f0e2fb41eeea6b83d62e57
Unknown <ahallmann22@gmail.com>
2019-01-29 19:03:27 +0100

    CNN: Fehler ausgemerzt

--- CNN/cnn_trainingscenter.py
+++ CNN/cnn_trainingscenter.py
@@ -8,10 +8,15 @@ from keras.utils import to_categorical
 X_train_all, y_train_all = mnist_reader.load_mnist('../MNISTdataset', kind='train')
 X_test_all, y_test_all = mnist_reader.load_mnist('../MNISTdataset', kind='t10k')
 
-X_train = X_train_all[0:600]
-y_train = y_train_all[0:600]
-X_test = X_test_all[0:100]
-y_test = y_test_all[0:100]
+#preprocessing training data
+row, col = 28, 28
+X_train_all = X_train_all.reshape(X_train_all.shape[0], row, col, 1)
+X_test_all = X_test_all.reshape(X_test_all.shape[0], row, col, 1)
+
+X_train = X_train_all[0:1800]
+y_train = y_train_all[0:1800]
+X_test = X_test_all[0:300]
+y_test = y_test_all[0:300]
 
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
@@ -19,14 +24,13 @@ X_test = X_test.astype('float32')
 X_train = X_train / 255.0
 X_test = X_test / 255.0
 
-#preprocessing training data
-row, col = 28, 28
+
 classes = 10
 
-X_train = X_train.reshape(X_train.shape[0], row * col)
-X_test = X_test.reshape(X_test.shape[0], row * col)
+
 print('X_train: ', X_train.shape)
 print('X_test: ',X_test.shape)
+
 
 y_train = to_categorical(y_train, classes)
 y_test = to_categorical(y_test, classes)
@@ -34,6 +38,6 @@ print('y_train: ',y_train.shape)
 print('X_test: ',y_test.shape)
 
 own_cnn(X_train_all, y_train_all, X_test_all, y_test_all, X_train, y_train, X_test, y_test, classes=10,
-            batch_size=128, epochs=50, num_conv_layer_per_pooling=2, num_of_poolings=4,
+            batch_size=64, epochs=30, num_conv_layer_per_pooling=2, num_of_poolings=4,
             pool_size=2, kernel_size=3, padding='same', activation='relu',
-            list_of_kernel_numbers=[64, 128, 256, 512], dense_layers=2, neurons_in_dense_layer=4096, dropout=0)
+            list_of_kernel_numbers=[32, 64, 128, 256], dense_layers=4, neurons_in_dense_layer=1024, dropout=1)
--- CNN/own_cnn.py
+++ CNN/own_cnn.py
@@ -2,9 +2,10 @@ import matplotlib.pyplot as plt
 from keras.layers import Convolution2D, MaxPooling2D, Dense, Dropout, Flatten
 from keras.models import Sequential
 import numpy as np
+from keras.callbacks import CSVLogger
 
 def own_cnn(X_train_all, y_train_all, X_test_all, y_test_all, X_train, y_train, X_test, y_test, classes=10,
-            batch_size=128, epochs=50, num_conv_layer_per_pooling=2, num_of_poolings=4,
+            batch_size=128, epochs=50, num_conv_layer_per_pooling=2, num_of_poolings=3,
             pool_size=2, kernel_size=3, padding='same', activation='relu',
             list_of_kernel_numbers=[64, 128, 256, 512], dense_layers=2, neurons_in_dense_layer=4096, dropout=0):
     """sklearn_svm(...)
@@ -39,18 +40,24 @@ def own_cnn(X_train_all, y_train_all, X_test_all, y_test_all, X_train, y_train, 
 
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-    csv_logger = CSVLogger('training.log', separator=',', append=True)
+    log_datei = 'training.log'
+    csv_logger = CSVLogger(log_datei, separator=',', append=True)
 
     hist = model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs,
-            verbose=1, validation_data=(X_train, y_train), callbacks=[csv_logger])
-    '''
+            verbose=1, validation_data=(X_test, y_test), callbacks=[csv_logger])
+
+
+
+    #evaluate
+    score = model.evaluate(X_test, y_test, batch_size)
+    model.summary() # prints Summary on console
+    print('Score:')
+    print(model.metrics_names[0], model.metrics_names[1])
+    print(score[0], score[1])
+
+    f = open(log_datei, 'a')
+    f.write('\n')
+    f.close()
     plt.plot(hist.history['val_acc'])
     plt.plot(hist.history['loss'])
     plt.show()
-    '''
-
-    #evaluate
-    score = model.evaluate(X_test, y_test, batch_size, )
-    summary = model.summary()
-    print(score)
-    print(summary)
