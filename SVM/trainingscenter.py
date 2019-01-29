aff13fc2d546525832fb6d222db1cae25b565437
Unknown <ahallmann22@gmail.com>
2019-01-29 12:15:17 +0100

    svm: Trainingscenter angepasst

--- SVM/trainingscenter.py
+++ SVM/trainingscenter.py
@@ -11,13 +11,13 @@ from sklearn_svm import sklearn_svm
 
 # loading and preprocessing images
 
-X_train, y_train = mnist_reader.load_mnist('../MNISTdataset', kind='train')
-X_test, y_test = mnist_reader.load_mnist('../MNISTdataset', kind='t10k')
+X_trainALL, y_trainALL = mnist_reader.load_mnist('../MNISTdataset', kind='train')
+X_testALL, y_testALL = mnist_reader.load_mnist('../MNISTdataset', kind='t10k')
 
-X_train = X_train[0:18000]
-y_train = y_train[0:18000]
-X_test = X_test[0:3000]
-y_test = y_test[0:3000]
+X_train = X_trainALL[0:18000]
+y_train = y_trainALL[0:18000]
+X_test = X_testALL[0:3000]
+y_test = y_testALL[0:3000]
 
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
@@ -30,110 +30,26 @@ print("Testing matrix shape", X_test.shape)
 
 # Trainiere und Teste SVM
 
-# Kernel: linear
-sklearn_svm(X_train, y_train, X_test, y_test, C=1.0, kernel='linear', degree=2, gamma=1,
-                 coef0=0.0, shrinking=True, probability=False,
-                 tol=1e-3, cache_size=4000)
-
-sklearn_svm(X_train, y_train, X_test, y_test, C=1.0, kernel='linear', degree=2, gamma=1,
-                 coef0=1.0, shrinking=True, probability=False,
-                 tol=1e-3, cache_size=4000)
-
-sklearn_svm(X_train, y_train, X_test, y_test, C=1.0, kernel='linear', degree=2, gamma=1,
-                 coef0=10.0, shrinking=True, probability=False,
-                 tol=1e-3, cache_size=4000)
-
-sklearn_svm(X_train, y_train, X_test, y_test, C=1.0, kernel='linear', degree=2, gamma=1,
-                 coef0=100.0, shrinking=True, probability=False,
-                 tol=1e-3, cache_size=4000)
-
-sklearn_svm(X_train, y_train, X_test, y_test, C=1.0, kernel='linear', degree=2, gamma=10,
-                 coef0=0.0, shrinking=True, probability=False,
-                 tol=1e-5, cache_size=4000)
-
 # kernel: poly
 
 # degree: 2
-sklearn_svm(X_train, y_train, X_test, y_test, C=1.0, kernel='poly', degree=2, gamma=1,
-                 coef0=0.0, shrinking=True, probability=False,
+for n in np.arange(1,51):
+    sklearn_svm(X_train, y_train, X_test, y_test, C=1.0, kernel='poly', degree=2, gamma=n*0.004,
+                 coef0=60, shrinking=True, probability=False,
                  tol=1e-3, cache_size=4000)
 
-sklearn_svm(X_train, y_train, X_test, y_test, C=1.0, kernel='poly', degree=2, gamma=1,
-                 coef0=1.0, shrinking=True, probability=False,
+for n in np.arange(1,51):
+    sklearn_svm(X_train, y_train, X_test, y_test, C=1.0, kernel='poly', degree=2, gamma=0.004,
+                 coef0=n*4, shrinking=True, probability=False,
                  tol=1e-3, cache_size=4000)
 
-sklearn_svm(X_train, y_train, X_test, y_test, C=1.0, kernel='poly', degree=2, gamma=1,
-                 coef0=10.0, shrinking=True, probability=False,
-                 tol=1e-3, cache_size=4000)
+for n in np.arange(1,101):
+    X_train = X_trainALL[0:n*600]
+    y_train = y_trainALL[0:n*600]
+    X_test = X_testALL[0:n*100]
+    y_test = y_testALL[0:n*100]
 
-sklearn_svm(X_train, y_train, X_test, y_test, C=1.0, kernel='poly', degree=2, gamma=1,
-                 coef0=100.0, shrinking=True, probability=False,
-                 tol=1e-3, cache_size=4000)
-
-sklearn_svm(X_train, y_train, X_test, y_test, C=1.0, kernel='poly', degree=2, gamma=10,
-                 coef0=0.0, shrinking=True, probability=False,
-                 tol=1e-3, cache_size=4000)
-
-# degree: 4
-sklearn_svm(X_train, y_train, X_test, y_test, C=1.0, kernel='poly', degree=4, gamma=1,
-                 coef0=0.0, shrinking=True, probability=False,
-                 tol=1e-3, cache_size=4000)
-
-sklearn_svm(X_train, y_train, X_test, y_test, C=1.0, kernel='poly', degree=4, gamma=1,
-                 coef0=1.0, shrinking=True, probability=False,
-                 tol=1e-3, cache_size=4000)
-
-sklearn_svm(X_train, y_train, X_test, y_test, C=1.0, kernel='poly', degree=4, gamma=1,
-                 coef0=10.0, shrinking=True, probability=False,
-                 tol=1e-3, cache_size=4000)
-
-sklearn_svm(X_train, y_train, X_test, y_test, C=1.0, kernel='poly', degree=4, gamma=1,
-                 coef0=100.0, shrinking=True, probability=False,
-                 tol=1e-3, cache_size=4000)
-
-sklearn_svm(X_train, y_train, X_test, y_test, C=1.0, kernel='poly', degree=4, gamma=10,
-                 coef0=0.0, shrinking=True, probability=False,
-                 tol=1e-3, cache_size=4000)
-
-sklearn_svm(X_train, y_train, X_test, y_test, C=1.0, kernel='poly', degree=4, gamma=0.01,
-                 coef0=1.0, shrinking=True, probability=False,
-                 tol=1e-3, cache_size=4000)
-
-sklearn_svm(X_train, y_train, X_test, y_test, C=1.0, kernel='poly', degree=4, gamma=0.001,
-                 coef0=1.0, shrinking=True, probability=False,
-                 tol=1e-3, cache_size=4000)
-
-# degree : 9
-sklearn_svm(X_train, y_train, X_test, y_test, C=1.0, kernel='poly', degree=9, gamma=1,
-                 coef0=0.0, shrinking=True, probability=False,
-                 tol=1e-3, cache_size=4000)
-
-sklearn_svm(X_train, y_train, X_test, y_test, C=1.0, kernel='poly', degree=9, gamma=10,
-                 coef0=0.0, shrinking=True, probability=False,
-                 tol=1e-3, cache_size=4000)
-
-sklearn_svm(X_train, y_train, X_test, y_test, C=1.0, kernel='poly', degree=9, gamma=0.01,
-                 coef0=0.0, shrinking=True, probability=False,
-                 tol=1e-3, cache_size=4000)
-
-sklearn_svm(X_train, y_train, X_test, y_test, C=1.0, kernel='poly', degree=9, gamma=0.001,
-                 coef0=0.0, shrinking=True, probability=False,
-                 tol=1e-3, cache_size=4000)
-
-#kernel: rbf
-sklearn_svm(X_train, y_train, X_test, y_test, C=1.0, kernel='rbf', degree=9, gamma=1,
-                 coef0=0.0, shrinking=True, probability=False,
-                 tol=1e-3, cache_size=4000)
-
-sklearn_svm(X_train, y_train, X_test, y_test, C=1.0, kernel='rbf', degree=9, gamma=10,
-                 coef0=0.0, shrinking=True, probability=False,
-                 tol=1e-3, cache_size=4000)
-
-sklearn_svm(X_train, y_train, X_test, y_test, C=1.0, kernel='rbf', degree=9, gamma=0.01,
-                 coef0=0.0, shrinking=True, probability=False,
-                 tol=1e-3, cache_size=4000)
-
-sklearn_svm(X_train, y_train, X_test, y_test, C=1.0, kernel='rbf', degree=9, gamma=0.001,
-                 coef0=0.0, shrinking=True, probability=False,
-                 tol=1e-3, cache_size=4000)
+    sklearn_svm(X_train, y_train, X_test, y_test, C=1.0, kernel='poly', degree=2, gamma=0.05,
+                     coef0=0.0, shrinking=True, probability=False,
+                     tol=1e-3, cache_size=4000)
 
