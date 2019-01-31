861ca436f41924dea364e45d2edc6814ccee84d1
cataleya <katkrebs@web.de>
2019-01-31 16:42:36 +0100

    nn data added

---
+++ NN/nn1/mnist-nn1_30_60000.py
@@ -0,0 +1,88 @@
+from keras.datasets import mnist
+from keras.preprocessing.image import load_img, array_to_img
+from keras.utils import to_categorical
+from keras.models import Sequential
+#fully connected layer Dense
+from keras.layers import Dense 
+#from keras.utils import plot_model
+from keras.callbacks import CSVLogger
+
+from matplotlib import pyplot as plt
+import numpy as np
+
+def plot_model_history(history):
+    # list all data in history
+    print(history.history.keys())
+    # summarize history for accuracy
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+    # summarize history for loss
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+(X_train_all, y_train_all), (X_test_all, y_test_all) = mnist.load_data()
+#print(X_train.shape)
+#print(y_train.shape)
+#print(X_test.shape)
+#print(y_test.shape)
+#print(X_train[0])
+
+X_train = X_train_all
+y_train = y_train_all
+X_test = X_test_all
+y_test = y_test_all
+
+
+#preprocessing training data
+row, col = 28, 28
+X_train = X_train.reshape(3000, row * col)
+X_test = X_test.reshape(1000, row * col)
+
+print(X_train.shape)
+print(X_test.shape)
+
+#values from range 0-255 change to range 0-1
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_test2 = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+
+#print(X_train[0], )
+
+#preprocessing test data
+classes = 10
+y_train = to_categorical(y_train, classes)
+y_test = to_categorical(y_test, classes)
+
+
+
+#building model
+
+nn1 = Sequential()
+nn1.add(Dense(1000, activation='relu', input_shape=(784,)))
+nn1.add(Dense(500, activation ='relu'))
+nn1.add(Dense(classes, activation='softmax'))
+nn1.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+print(nn1.summary())
+
+csv_logger = CSVLogger('training2.log', separator=',', append=False)
+
+nn1_history = nn1.fit(X_train, y_train, epochs=30, verbose=1, validation_data=(X_test, y_test), callbacks=[csv_logger])
+nn1_score = nn1.evaluate(X_test, y_test)
+
+print('Test score:', nn1_score[0])
+print('Test accuracy:', nn1_score[1])
+
+plot_model_history(nn1_history)
